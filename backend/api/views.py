d3f7a6a6f2c6609d5e96fb318fb249b9808a3894
Samonto Karmaker <samonto0726@gmail.com>
2025-09-05 11:50:05 +0600

    Implement views for listing, creating, and deleting user-specific notes

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,36 @@
 from rest_framework import generics
-from .serializers import UserSerializer
+
+from .models import Note
+from .serializers import UserSerializer, NoteSerializer
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 # Create your views here.
+# List of notes
+class NoteListView(generics.ListAPIView):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access this view
+
+# List notes of the authenticated user      
+class UserNotesListView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access this view
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)  # DRF already validates the serializer
+        
+class NoteDeleteView(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access this view
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 # DRF view itself handles the POST request, validation, and user creation
 class CreateUserView(generics.CreateAPIView):
